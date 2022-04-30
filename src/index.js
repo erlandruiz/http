eb648452704beaf85c86b0bc3e62862f118b8273
Erland <erlandruiz@hotmail.com>
2022-04-30 01:46:55 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// import * as CRUD from './js/crud-provider';
+
+import { init } from './js/archivos-page';
+
+init();
+// CRUD.getUsuario( 2 ).then( console.log );
+
+// CRUD.crearUsuario({
+//     name: 'Erland',
+//     job: 'Directional Driller'
+// }).then( console.log );
+
+// CRUD.actualizarUsuario( 1, {
+//     name: 'Melissa',
+//     job: 'Developer'
+// }).then(console.log)
+
+// CRUD.borrarUsuario(1).then(console.log);
